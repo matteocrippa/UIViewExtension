a00348a5bc9f7b0bba2a97e1d50e624646815b4d
Matteo <matteo@boostco.de>
2016-08-03 09:17:32 +0200

    added shadows

--- UIViewExtension/Classes/UIViewExtension.swift
+++ UIViewExtension/Classes/UIViewExtension.swift
@@ -29,4 +29,31 @@ extension UIView {
             layer.borderColor = newValue?.CGColor
         }
     }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(CGColor: layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowRadius: Double {
+        get {
+            return Double(self.layer.shadowRadius)
+        }
+        set {
+            self.layer.shadowRadius = CGFloat(newValue)
+        }
+    }
 }
